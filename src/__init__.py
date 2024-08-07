f0c05402317af5eab7555833ad4dd39dd995b3cc
devabz <devabzko@gmail.com>
2024-08-07 09:11:50 +0200

    Completed PER

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
 from src.utils import *
-from src.baseclasses import *
 from src.networks import *
 from src.memory import *
--- src/baseclasses/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.baseclasses.baseclasses import BaseMemory, BaseAgent
