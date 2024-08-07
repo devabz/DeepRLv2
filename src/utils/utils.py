800eea8a73caf8e50b85a6378583581250cc6abb
devabz <devabzko@gmail.com>
2024-08-07 03:22:21 +0200

    Added compilation function for mp4

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,10 @@
 import cv2
 import imageio
+import numpy as np
+
 
 def compile_to_mp4(frames, fps, path):
+    frames = np.array(frames).astype(np.uint8)
     height, width, layers = frames[0].shape
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -11,9 +14,16 @@ def compile_to_mp4(frames, fps, path):
         out.write(frame)
 
     out.release()
-
-def compile_to_gif(frames, fps, path):
-    duration = 1 / fps 
-    imageio.mimsave(path, frames, format='GIF', duration=duration)
     
-
+if __name__ == "__main__":
+    import time
+    import numpy as np
+    
+    
+    frames = np.random.randint(0, 255, size=(1000, 320, 320, 3))
+    
+    mp4_start_time = time.perf_counter()
+    compile_to_mp4(frames=frames, fps=30, path='video.mp4')
+    mp4_end_time = time.perf_counter() - mp4_start_time
+    
+    print(f'compiled {frames.shape} to mp4 in {round(mp4_end_time, 2)}')
